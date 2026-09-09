4e120d20261bf3d8979c83fb3218816309eb5840
Kushal <kushalmeghani108@gmail.com>
2026-09-09 22:28:02 +0530

    KUS-49: gofmt import alignment after module rename

--- internal/sandbox/runtime_test.go
+++ internal/sandbox/runtime_test.go
@@ -4,8 +4,8 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/goaudit/cli/internal/diagnostic"
 	"github.com/docker/docker/api/types/system"
+	"github.com/goaudit/cli/internal/diagnostic"
 )
 
 func TestRuntimeFromDockerInfo(t *testing.T) {
